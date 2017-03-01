58f4d759072e74cc6096c0928c425943d235410d
Mblades <mblades96@gmail.com>
2017-03-01 10:12:49 -0500

    Made pages look better

--- Pages/homePage.php
+++ Pages/homePage.php
@@ -12,53 +12,54 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Login/Register</title>
 
-    <!-- Bootstrap -->
-    <link href="lib/bootstrap/css/bootstrap.css" rel="stylesheet">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="lib/bootstrap/js/bootstrap.min.js"></script>
-	<script src="lib/Jquery/jquery-3.1.1.min.js"></script>
-	<script href="style/general.css" rel="stylesheet" type="text/css"></script>
 
-	
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link href="style/general.css" rel="stylesheet" type="text/css">
+ 
+
   </head>
-  <body background ="marist_pic2.jpg">	
+  <body>	
 	<div class="container">
+		<div class="row">
+            <div class="col-sm-6" id="currentUser">
+				<h3>Hi, Blades</h3>
+			</div>
+			<div class="col-sm-6" id="utilities">
+                <!-- Utility Icons Here -->
+                <a type="button" href="loginPage.php"><img src="img/home.png" width="40px" alt="Home" title="Home"></a>
+                <img src="img/profile.png" width="40px" alt="Profile" title="Profile">
+                <img src="img/settings.png" width="40px" alt="Settings" title="Settings">
+            </div>
+        </div>
 		<div class="page-header text-center" id="pg_header">
-			<h1><font face="Comic sans MS" size="40px" color="black">Marist Fox Trivia</font><br /></h1>
+			<h1><font face="Comic sans MS" color="black">Marist Fox Trivia</font><br /></h1>
 		</div>
 		<div class="row">
-			<div class="col-sm-6 col-md-4">
-				<div class="thumbnail">
-					<img src="marist_pic3.jpg">
-					<div class="caption">
-						<h3>Math</h3>
-						<p>Quick description of the game</p>
-						<p><a href="#" class="btn btn-primary" role="button">Play</a></p>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-6 col-md-4">
-				<div class="thumbnail">
-					<img src="marist_pic3.jpg">
-					<div class="caption">
-						<h3>Business</h3>
-						<p>Quick description of the game</p>
-						<p><a href="#" class="btn btn-primary" role="button">Play</a></p>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-6 col-md-4">
-				<div class="thumbnail">
-					<img src="marist_pic3.jpg">
-					<div class="caption">
-						<h3>Science</h3>
-						<p>Quick description of the game</p>
-						<p><a href="#" class="btn btn-primary" role="button">Play</a></p>
-					</div>
-				</div>
-			</div>
+		<?php
+                                $numberOfGames = 6;
+                                //$categories = ["Social Studies", "Science", "Math", "English", "PhysEd", "Health", "Art", "Music", "Business", "Computers"];
+                                //$colors = ["#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#f1c40f", "#e67e22", "#e74c3c", "#ecf0f1", "#16a085", "#27ae60", "#2980b9", "#8e44ad", "#f39c12", "#d35400", "#c0392b", "#bdc3c7"];
+                                //$apothem = 250;
+                                //$base = (2 * $apothem) * tan(deg2rad(180 / $numberOfCategories));
+                                //$borderWidth = "border-width:" . $apothem . "px " . ($base / 2) . "px 0 " . ($base / 2) . "px;";
+                                //$transformOrigin = "transform-origin: " . ($base / 2) . "px " . $apothem . "px;";
+                                //$marginLeft = "margin-left: " . (250 - ($base / 2)) . "px;";
+
+                                for ($i = 0; $i < $numberOfGames; $i++) {
+	                                echo "<div class='col-sm-6 col-md-3'>
+											<div class='thumbnail'>
+												<img src='img/marist_pic3.jpg'>
+												<div class='caption'>
+													<h3>Math</h3>
+													<p>Quick description of the game</p>
+													<p><a href='#' class='btn btn-primary' role='button'>Play</a></p>
+												</div>
+											</div>
+										</div>";
+								}
+        ?>
 		</div>
 	</div>    
 
---
+++ Pages/index.php
@@ -0,0 +1,43 @@
+<?php
+	//page title
+	$title = 'LoginPage';
+	//Landing page
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>LandingPage</title>
+
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link href="style/landing.css" rel="stylesheet" type="text/css">
+ 
+  </head>
+  <body>
+	<header id="top" class="header">
+        <div class="text-vertical-center">
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<div class="col-sm-3">
+			</div>
+				<div class="col-sm-6">
+					<div class="well well-lg">
+						<h1>Marist Fox Trivia</h1>
+						<h3>Get Ready To Study</h3>
+						<br>
+						<a href="loginPage.php" class="btn btn-success btn-lg">Login/Register</a>
+					</div>
+				</div>
+			
+        </div>
+    </header>
+  </body>
+</html>
