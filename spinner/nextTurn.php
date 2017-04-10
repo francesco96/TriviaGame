baa394885c58bde587e90a2ec0e74251939fb7fb
Mblades <mblades96@gmail.com>
2017-04-10 15:13:07 -0400

    update

--- spinner/nextTurn.php
+++ spinner/nextTurn.php
@@ -7,10 +7,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$sql = "SELECT * FROM game_session WHERE SESSION_ID='$sid';";
 		$result = mysqli_query($conn, $sql);
 		$result = mysqli_fetch_assoc($result);
+		$cid= $result['COURSE_ID'];
 		$sql = "UPDATE score SET SCORE = $score WHERE SESSION_ID = $sid AND USER_ID = $uid";
 		mysqli_query($conn, $sql);
-		$sql = "UPDATE game_session SET USER_ID_WINNER='0' WHERE `SESSION_ID`='$sid'";
+		if($uid == $result['USER_ID_2']){
+			$sql = "UPDATE game_session SET USER_ID_WINNER='". $result['USER_ID_1'] ."' WHERE `SESSION_ID`='$sid'";
+		}else {
+			$sql = "UPDATE game_session SET USER_ID_WINNER='". $result['USER_ID_2'] ."' WHERE `SESSION_ID`='$sid'";
+		}
 		mysqli_query($conn, $sql);
-		header("Location: ../homePage.php");
+		header("Location: ../gameinfo.php?cid=$cid");
 }
 ?>
