5a93ddc6b76e68ec6305e49f4b5ee7f9cb8ba3b8
Mblades <mblades96@gmail.com>
2017-04-24 10:00:51 -0400

    starting a session score is updated

--- startSession.php
+++ startSession.php
@@ -3,21 +3,35 @@ include ('db.php');
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$userID = $_SESSION['userId'];
 	$cid = $_POST['cid'];
-	$sql = "SELECT * FROM game_session WHERE USER_ID_2 = 0 AND USER_ID_1 != $userID LIMIT 1";
+	$sql = "SELECT * FROM game_session WHERE USER_ID_2 = 0 AND USER_ID_1 != $userID LIMIT 1 AND USER_ID_WINNER = 0";
 	$result = mysqli_query($conn, $sql);
+	$sql = "SELECT * FROM categorylist WHERE $cid = COURSE_ID";
+	$result2 = mysqli_query($conn, $sql);
 	if (mysqli_num_rows($result) > 0) {
 		$result = mysqli_fetch_assoc($result);
 		$sql= "UPDATE game_session SET USER_ID_2 = $userID, USER_ID_WINNER = $userID WHERE SESSION_ID = " . $result['SESSION_ID'];
 		mysqli_query($conn, $sql);
-		$sql = "INSERT INTO score (USER_ID, SCORE, SESSION_ID) VALUES ('$userID', '0', '". $result['SESSION_ID'] ."');";
-		mysqli_query($conn, $sql);
+		//need to update how score is created to intclude all of the categories
+		
+		if(mysqli_num_rows($result2) > 0){
+			while($row = mysqli_fetch_assoc($result2)) {
+				$sql = "INSERT INTO score (USER_ID,CATEGORY_ID, SESSION_ID) VALUES ($userID, ". $row['CATEGORY_ID'] .", ". $result['SESSION_ID'] .")";
+				//echo $sql;
+				mysqli_query($conn, $sql);
+			}
+		}
 		header("Location: spinner/index.php?sid=". $result['SESSION_ID']);
 	}else {
 		$sql = "INSERT INTO game_session (USER_ID_1, USER_ID_2, USER_ID_WINNER, COURSE_ID, IS_OVER) VALUES ('$userID', '0', '$userID', '$cid', '1');";
 		mysqli_query($conn, $sql);
 		$sid = $conn->insert_id;
-		$sql = "INSERT INTO score (USER_ID, SCORE, SESSION_ID) VALUES ('$userID', '0', '$sid');";
-		mysqli_query($conn, $sql);
+		if(mysqli_num_rows($result2) > 0){
+			while($row = mysqli_fetch_assoc($result2)) {
+				$sql = "INSERT INTO score (USER_ID,CATEGORY_ID, SESSION_ID) VALUES ($userID, ". $row['CATEGORY_ID'] .", ". $sid .");";
+				mysqli_query($conn, $sql);
+				
+			}
+		}
 		header("Location: spinner/index.php?sid=$sid");
 	}
 }
